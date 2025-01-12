4a0fe11ebe56d6670f090beb6e180a1cee96580d
Axel Lanty <alanty@c2r9s5.42luxembourg.lu>
2025-01-12 04:03:22 +0100

    plus jamais je push a 4h03

---
+++ CPP_05/ex01/Form.hpp
@@ -0,0 +1,44 @@
+#ifndef FORM_CLASS_H
+# define FORM_CLASS_H
+
+# include <iostream>
+# include <string>
+# include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form {
+private:
+	const std::string	_name;
+	bool				_isSigned;
+	const unsigned int	_signGrade;
+	const unsigned int	_execGrade;
+
+public:
+	Form(void);
+	Form(std::string name, unsigned int signGrade, unsigned int execGrade);
+	Form(const Form &src);
+	~Form(void);
+	Form	&operator =(const Form &src);
+
+	std::string		getName(void) const;
+	bool			isSigned(void) const;
+	unsigned int	getSignGrade(void) const;
+	unsigned int	getExecGrade(void) const;
+
+	void			beSigned(const Bureaucrat bur);
+
+	class GradeTooHighException: public std::exception {
+	public:
+		virtual const char* what(void) const throw();
+	};
+
+	class GradeTooLowException: public std::exception {
+	public:
+		virtual const char* what(void) const throw();
+	};
+};
+
+std::ostream	&operator <<(std::ostream &o, const Form &form);
+
+#endif
