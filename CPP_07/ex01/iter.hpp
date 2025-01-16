b50c8acaa9eabe0afa7fcca3c939f3043dc66833
Axel Lanty <alanty@c1r8s3.42luxembourg.lu>
2025-01-16 11:26:20 +0100

    MAIS CA MARCHE

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -4,7 +4,16 @@
 template <typename T>
 void iter(T* array, int length, void (*func)(T&))
 {
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < length; ++i) 
+    {
+        func(array[i]);
+    }
+}
+
+template <typename T>
+void iter(const T* array, int length, void (*func)(const T&)) 
+{
+    for (int i = 0; i < length; ++i)
     {
         func(array[i]);
     }
--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -1,53 +1,25 @@
 #include <iostream>
-#include <Array.hpp>
+#include "iter.hpp"
 
 #define MAX_VAL 750
-int main(int, char**)
-{
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
+
+void increment(int& x) {
+    x++;
+}
+
+void print(const int& x) {
+    std::cout << x << " ";
+}
+
+int main() {
+    int array[MAX_VAL];
+    for (int i = 0; i < MAX_VAL; ++i) {
+        array[i] = i;
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    iter(array, MAX_VAL, increment);
+    iter(array, MAX_VAL, print);
+    std::cout << std::endl;
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
     return 0;
 }
